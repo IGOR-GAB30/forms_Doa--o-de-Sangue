d4e06db91b7cda0547ebc3b904c6003095e9ef55
IGOR-GAB30 <igor_paradella@estudante.sesisenai.org.br>
2026-04-05 18:37:55 -0300

    ADD - Acrescentei todos os parâmetros obrigatórios para cada requisito dentro do javascript utilizando o if.

--- script.js
+++ script.js
@@ -9,8 +9,74 @@ form.addEventListener('submit', function (e) {
     let nome = document.getElementById('nome').value;
     let idade = document.getElementById('idade').value;
     let peso = document.getElementById('peso').value;
-    let tipoSanguineo = document.getElementById('tipoSanguineo').value;
+    let tipoSanguineo = document.getElementById('tipo_sanguineo').value;
     let email = document.getElementById('email').value;
     let telefone = document.getElementById('telefone').value;
     let estado = document.getElementById('estado').value;
     let cidade = document.getElementById('cidade').value;
+
+    
+   if (nome === '') {
+        alert('Por favor, preencha o campo nome.');
+        valido = false;
+    }
+
+    if (idade === '' || idade < 16) {
+        alert('Por favor, preencha o campo idade e certifique-se de que tenha ao menos 16 anos.');
+        valido = false;
+    }
+
+    if (peso === '' || peso < 50) {
+        alert('Por favor, preencha o campo peso e certifique-se de que tenha ao menos 50 kg.');
+        valido = false;
+    }
+
+    if (tipoSanguineo === '') {
+        alert('Por favor, selecione um tipo sanguíneo.');
+        valido = false;
+    }
+
+    if (email === ''|| !email.includes('@') || !email.includes('.') || email.length < 5) {
+        alert('Por favor, preencha o campo email.');
+        valido = false;
+    }
+
+    if (telefone === '' || telefone.length < 10) {
+        alert('Por favor, preencha o campo telefone com um número válido.');
+        valido = false;
+    }
+
+    if (estado === '') {
+        alert('Por favor, selecione um estado.');
+        valido = false;
+    }
+
+    if (cidade === '' || cidade.length < 3) {
+        alert('Por favor, preencha o campo cidade.');
+        valido = false;
+    }  
+
+    if (valido) {
+
+        alert('Formulário enviado com sucesso!');
+
+         let resultado = document.getElementById('resultado');
+
+        resultado.innerHTML = `
+
+        Dados enviados: <br>
+        Nome: ${nome} <br>
+        Idade: ${idade} <br>
+        Peso: ${peso} kg <br>
+        Tipo Sanguíneo: ${tipoSanguineo} <br>
+        Email: ${email} <br>
+        Telefone: ${telefone} <br>
+        Estado: ${estado} <br>
+        Cidade: ${cidade} <br>
+        `;
+
+        form.reset();
+    }
+
+});
+
